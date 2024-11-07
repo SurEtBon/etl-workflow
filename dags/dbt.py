dc53de71faae57a1bb8b2f39db07d0fcdb0cc4f9
j-about <142051449+j-about@users.noreply.github.com>
2024-11-07 00:11:28 +0000

    remove google maps platform trigger of dbt dag

--- dags/dbt.py
+++ dags/dbt.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 DBT_DIR = os.getenv("DBT_DIR")
 
@@ -24,10 +23,4 @@ with DAG(
         bash_command=f"dbt test --project-dir {DBT_DIR}",
     )
 
-    trigger_google_maps_platform = TriggerDagRunOperator(
-        task_id = "trigger_google_maps_platform",
-        trigger_dag_id = "google_maps_platform",
-        dag = dag
-    )
-
-    dbt_run >> dbt_test >> trigger_google_maps_platform
+    dbt_run >> dbt_test
